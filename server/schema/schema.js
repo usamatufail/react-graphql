c00b3ace797142cafb8d0dfa75788e5a1976856a
Sam Tufail <miansamusama@gmail.com>
2021-02-22 00:17:01 +0500

    feat(main): initial commit with schema

---
+++ server/schema/schema.js
@@ -0,0 +1,56 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
+
+// dummy data
+const books = [
+  { name: 'Bang e Dara', genre: 'Poetry', id: '1' },
+  { name: 'Zarb e Kaleem', genre: 'Poetry', id: '2' },
+]
+
+const authors = [
+  { name: 'Allama Muhammad Iqbal', age: 66, id: '1' }
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    age: { type: GraphQLInt },
+    name: { type: GraphQLString }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db
+        return _.find(books, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
